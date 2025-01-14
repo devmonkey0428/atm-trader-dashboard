dd3e747cf2383e1a4e1a384d1f9763fee862fde9
devmonkey0428 <devmonkey0428@gmail.com>
2025-01-14 15:53:25 -0800

    fix: isEmbedded function check again

--- frontend/src/embedValidator.ts
+++ frontend/src/embedValidator.ts
@@ -1,11 +1,11 @@
 export function validateEmbedding() {
-    // const allowedOrigins = [ 'http://18.225.2.2', 'http://wireless.atmtrader.com', 'https://wireless.atmtrader.com', 'https://atmtrader.com'];
-    // const currentOrigin = window.location.origin;
+    const allowedOrigins = [ 'http://18.225.2.2', 'http://wireless.atmtrader.com', 'https://wireless.atmtrader.com', 'https://atmtrader.com'];
+    const currentOrigin = window.location.origin;
 
-    // if (!allowedOrigins.includes(currentOrigin)) {
-    //     document.body.innerHTML = '<h1>Unauthorized Embedding</h1>';
-    //     return false;
-    // }
+    if (!allowedOrigins.includes(currentOrigin)) {
+        document.body.innerHTML = '<h1>Unauthorized Embedding</h1>';
+        return false;
+    }
 
     return true;
 }
